d43fee664ca9d54c850ec0b0fef337000606efef
Vero Asghar <james.asghar@gmail.com>
2023-09-19 19:12:09 +0200

    Added mach-glfw, mach-gpu, and cimgui for zig

--- build.zig
+++ build.zig
@@ -1,10 +1,9 @@
 const std = @import("std");
-const mach_core = @import("mach_core");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) !void {
+pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -16,24 +15,61 @@ pub fn build(b: *std.Build) !void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const mach_core_dep = b.dependency("mach_core", .{
+    const exe = b.addExecutable(.{
+        .name = "rendering-engine",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    const app = try mach_core.App.init(b, mach_core_dep.builder, .{
-        .name = "myapp",
-        .src = "src/main.zig",
+
+    const cimgui_dep = b.anonymousDependency("vendor/cimgui", @import("vendor/cimgui/build.zig"), .{
         .target = target,
         .optimize = optimize,
-        .deps = &[_]std.build.ModuleDependency{},
     });
-    if (b.args) |args| app.run.addArgs(args);
+
+    exe.linkLibrary(cimgui_dep.artifact("cimgui"));
+
+    exe.addModule("mach-gpu", b.dependency("mach_gpu", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("mach-gpu"));
+
+    const glfw_dep = b.dependency("mach_glfw", .{
+        .target = exe.target,
+        .optimize = exe.optimize,
+    });
+    exe.addModule("mach-glfw", glfw_dep.module("mach-glfw"));
+    try @import("mach_glfw").link(glfw_dep.builder, exe);
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build run`
     // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&app.run.step);
+    run_step.dependOn(&run_cmd.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -43,10 +79,11 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
-    const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }
